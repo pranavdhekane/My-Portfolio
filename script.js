eac4d3f2498026a44ed1f1ccc974e16ef929e160
pranavdhekane <pranavdhekane006@gmail.com>
2025-03-31 09:53:39 +0530

    changes

---
+++ script.js
@@ -0,0 +1,21 @@
+// let mode = document.querySelector(".logo");
+// let body = document.querySelector("body");
+// let intro = document.querySelector("#intro");
+// let contact = document.querySelector("#contact");
+// let msg = document.querySelector(".msg");
+
+
+// // body.style.backgroundColor = "rgb(15, 5, 18)";
+
+// mode.addEventListener("click", ()=> {
+//     if(body.style.backgroundColor == "rgb(5, 7, 10)"){
+//         body.style.backgroundColor = "white";
+//         contact.style.backgroundColor = "white";
+//         intro.style.color = "black";
+//         msg.style.color = "black";
+//     } else {
+//         body.style.backgroundColor = "rgb(5, 7, 10)";
+//         contact.style.backgroundColor = "rgb(5, 7, 10)";
+//         msg.style.color = "white";
+//     }
+// })
